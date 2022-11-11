4b6b21901ff9fe104ffc8817d726b1fc2d7e222f
erjav <erja.vaskivuori@helsinki.fi>
2022-11-11 15:24:09 +0200

    lisätty testejä unicafe-maksukortille

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,23 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_saldon_oikein(self):
+        self.maksukortti.lataa_rahaa(100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 11.00 euroa")
+
+    def test_saldo_vähenee_oikein(self):
+        self.maksukortti.ota_rahaa(100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 9.00 euroa")
+
+    def test_saldo_ei_muutu_negatiiviseksi(self):
+        self.maksukortti.ota_rahaa(1100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
