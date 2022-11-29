ab1361405c9d53b4decd037823b41c68193d908a
erjav <erja.vaskivuori@helsinki.fi>
2022-11-29 17:19:13 +0200

    Added tasks for testing and auto formatting

--- study-app/tasks.py
+++ study-app/tasks.py
@@ -5,6 +5,19 @@ def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
 
 @task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
+
+
+@task
+def format(ctx):  # pylint: disable=redefined-builtin
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest", pty=True)
 
