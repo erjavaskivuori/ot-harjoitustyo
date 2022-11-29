aaecbe191d3addef4d437516b8fee0e24c642b83
erjav <erja.vaskivuori@helsinki.fi>
2022-11-29 16:44:40 +0200

    Auto formatted and added missing brackets

--- study-app/src/repositories/task_repository.py
+++ study-app/src/repositories/task_repository.py
@@ -2,8 +2,9 @@ from database_connection import form_database_connection
 from entities.task import Task
 from entities.course import Course
 
+
 class TaskRepository:
-    
+
     def __init__(self, connection: form_database_connection):
 
         self._connection = connection
@@ -12,8 +13,8 @@ class TaskRepository:
 
         cursor = self._connection.cursor()
         cursor.execute("""INSERT INTO courseTasks (course_id, description
-                        visibility VALUES (?, ?, ?)""", 
-                        [course.id, description, 1])
+                        visibility VALUES (?, ?, ?)""",
+                       [course.id, description, 1])
         self._connection.commit()
 
         return Task(cursor.lastrowid, course, description, 1)
@@ -21,7 +22,8 @@ class TaskRepository:
     def get_tasks_by_course(self, course: Course):
 
         cursor = self._connection.cursor()
-        cursor.execute("SELECT * FROM courseTasks WHERE course_id=?", [course.id])
+        cursor.execute(
+            "SELECT * FROM courseTasks WHERE visibility=1 AND course_id=?", [course.id])
         rows = cursor.fetchall()
 
         return [Task(row(0), course, row[2], row(3)) for row in rows]
@@ -29,7 +31,9 @@ class TaskRepository:
     def remove_task(self, task: Task):
 
         cursor = self._connection.cursor()
-        cursor.execute("UPDATE courseTasks SET visibility=0 WHERE id=?", [task.id])
+        cursor.execute(
+            "UPDATE courseTasks SET visibility=0 WHERE id=?", [task.id])
         self._connection.commit()
 
-task_repository = TaskRepository(form_database_connection)
+
+task_repository = TaskRepository(form_database_connection())
