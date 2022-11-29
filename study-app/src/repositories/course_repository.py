f88a55b3258d6379f17f77c2ab9485441b321daf
erjav <erja.vaskivuori@helsinki.fi>
2022-11-29 16:44:20 +0200

    Auto formatted and added missing brackets

--- study-app/src/repositories/course_repository.py
+++ study-app/src/repositories/course_repository.py
@@ -2,6 +2,7 @@ from database_connection import form_database_connection
 from entities.user import User
 from entities.course import Course
 
+
 class CourseRepository:
 
     def __init__(self, connection: form_database_connection):
@@ -9,7 +10,7 @@ class CourseRepository:
         self._connection = connection
 
     def create_course(self, owner: User, name: str):
-        
+
         cursor = self._connection.cursor()
         cursor.execute("""INSERT INTO courses (user_id, name, visibility) 
                         VALUES (?, ?, ?)""", [owner.id, name, 1])
@@ -20,15 +21,17 @@ class CourseRepository:
     def get_users_courses(self, user: User):
 
         cursor = self._connection.cursor()
-        cursor.execute("SELECT * FROM courses WHERE user_id = ?", [user.id])
+        cursor.execute("SELECT * FROM courses WHERE visibility=1 AND user_id=?", [user.id])
         rows = cursor.fetchall()
 
         return [Course(row[0], user, row[2], row[3]) for row in rows]
 
     def remove_course(self, course: Course):
-        
+
         cursor = self._connection.cursor()
-        cursor.execute("UPDATE courses SET visibility=0 WHERE id=?", [course.id])
+        cursor.execute(
+            "UPDATE courses SET visibility=0 WHERE id=?", [course.id])
         self._connection.commit()
 
-course_repository = CourseRepository(form_database_connection)
+
+course_repository = CourseRepository(form_database_connection())
