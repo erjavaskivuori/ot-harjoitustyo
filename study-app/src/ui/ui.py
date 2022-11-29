b5058cc1c5d55c0ee73beaa3e2197d96ed13b65e
erjav <erja.vaskivuori@helsinki.fi>
2022-11-29 16:56:03 +0200

    Added views

--- study-app/src/ui/ui.py
+++ study-app/src/ui/ui.py
@@ -1,9 +1,74 @@
-#importit
+from ui.welcome_view import WelcomeView
+from ui.login_view import LoginView
+from ui.register_view import RegisterView
+from ui.all_courses_view import AllCoursesView
+from ui.course_view import CourseView
+
 
 class UI:
     def __init__(self, root):
         self._root = root
+        self._current_view = None
 
     def start(self):
+        self._show_welcome_view()
 
-        pass
+    def _hide_current_view(self):
+        if self._current_view:
+            self._current_view.destroy()
+        
+        self._current_view = None
+
+    def _show_welcome_view(self):
+        self._hide_current_view()
+
+        self._current_view = WelcomeView(
+            self._root,
+            self._show_login_view,
+            self._show_register_view
+        )
+
+        self._current_view.pack()
+
+    def _show_login_view(self):
+        self._hide_current_view()
+
+        self._current_view = LoginView(
+            self._root,
+            self._show_all_courses_view,
+            self._show_welcome_view
+        )
+
+        self._current_view.pack()
+
+    def _show_register_view(self):
+        self._hide_current_view()
+
+        self._current_view = RegisterView(
+            self._root,
+            self._show_all_courses_view,
+            self._show_welcome_view
+        )
+
+        self._current_view.pack()
+
+    def _show_all_courses_view(self):
+        self._hide_current_view()
+
+        self._current_view = AllCoursesView(
+            self._root,
+            self._show_welcome_view,
+            self._show_course_view
+        )
+
+        self._current_view.pack()
+
+    def _show_course_view(self):
+        self._hide_current_view()
+
+        self._current_view = CourseView(
+            self._root,
+            self._show_all_courses_view
+        )
+
+        self._current_view.pack()
