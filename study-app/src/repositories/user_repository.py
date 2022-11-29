76c8dbd00f37e9a2127892ed4229074a2ff9ca92
erjav <erja.vaskivuori@helsinki.fi>
2022-11-29 16:41:20 +0200

    Combined two functions for efficiency

--- study-app/src/repositories/user_repository.py
+++ study-app/src/repositories/user_repository.py
@@ -1,6 +1,7 @@
 from database_connection import form_database_connection
 from entities.user import User
 
+
 class UserRepository:
 
     def __init__(self, connection: form_database_connection):
@@ -17,32 +18,21 @@ class UserRepository:
 
     def find_by_username(self, username: str):
 
-        try:
-            cursor = self._connection.cursor()
-            cursor.execute("SELECT username FROM users WHERE username=?", [username])
-            return True
-        except:
-            return False
+        cursor = self._connection.cursor()
+        cursor.execute(
+            "SELECT username FROM users WHERE username=?", [username])
+        row = cursor.fetchall()
+
+        return User(row[0], row[1], row[2])
 
     def create_user(self, username: str, password: str):
-        
+
         cursor = self._connection.cursor()
         cursor.execute("""INSERT INTO users (username, password) 
                         VALUES (?, ?)""", [username, password])
         self._connection.commit()
 
         return User(cursor.lastrowid, username, password)
-
-    def find_password(self, username, password):
-
-        cursor = self._connection.cursor()
-        try:
-            cursor.execute("SELECT * FROM users WHERE username=?", [username])
-            row = cursor.fetchall()
-            if row[2] == password:
-                return User(row[0], row[1], row[2])    
-        except:
-            return False
 
     def delete_all_users(self):
         cursor = self._connection.cursor()
