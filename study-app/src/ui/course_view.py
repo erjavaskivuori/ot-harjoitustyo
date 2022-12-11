db9b33d24d26af9d8de9728082114407daeede81
erjav <erja.vaskivuori@helsinki.fi>
2022-12-11 17:27:00 +0200

    Increased functionality and fixed layout

--- study-app/src/ui/course_view.py
+++ study-app/src/ui/course_view.py
@@ -1,12 +1,17 @@
-from tkinter import ttk, constants, StringVar
+from tkinter import ttk, constants, messagebox
+from datetime import date
 from services.study_app_services import study_app_service
 
 
 class CourseView:
-    def __init__(self, root, show_all_courses_view):
+    def __init__(self, root, show_all_courses_view, show_create_task_view, show_task_view):
         self._root = root
         self._frame = None
         self._show_all_courses_view = show_all_courses_view
+        self._show_create_task_view = show_create_task_view
+        self._show_task_view = show_task_view
+        self._course = study_app_service.get_current_course()
+        self._course.tasks = study_app_service.get_tasks_by_course(self._course)
 
         self._initialize()
 
@@ -16,13 +21,89 @@ class CourseView:
     def destroy(self):
         self._frame.destroy()
 
+    def _initialize_task_entity(self, task):
+        
+        task_button = ttk.Button(
+            master=self._frame,
+            text= f"{task.title}",
+            command=lambda: [study_app_service.set_current_task(task), self._show_task_view()]
+        )
+
+        task_button.grid(padx=5, pady=5, sticky=constants.EW)
+
+    def _initialize_remove_course_popup(self):
+
+        msg_box = messagebox.askyesno(
+            title="Remove course",
+            message=f"Are you sure you want to remove {self._course.name} from your courses?",
+        )
+
+        if msg_box == True:
+            study_app_service.remove_course(self._course)
+            self._show_all_courses_view()
+
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
         label = ttk.Label(
             master=self._frame,
-            text=f"{study_app_service.get_current_course()}"
+            text=f"{self._course.name}"
         )
 
         self._frame.grid_columnconfigure(0, weight=1, minsize=400)
 
-        label.grid(row=0, sticky=constants.W)
+        label.grid(row=0, padx=5, pady= 5, sticky=constants.W)
+
+        current_date = date.today()
+
+        date_label = ttk.Label(
+            master=self._frame,
+            text=current_date
+        )
+
+        date_label.grid(row=0, padx=5, pady=5, sticky=constants.E)
+
+        add_task_button = ttk.Button(
+            master=self._frame,
+            text="Add task",
+            command=self._show_create_task_view
+        )
+
+        add_task_button.grid(sticky=constants.N)
+
+        todo_label = ttk.Label(
+            master=self._frame,
+            text="TO-DO:"
+        )
+
+        todo_label.grid(padx=5, pady=5, sticky=constants.W)
+
+        for task in self._course.tasks:
+            if task.state == 1:
+                self._initialize_task_entity(task)
+
+        done_label = ttk.Label(
+            master=self._frame,
+            text="\nCompleted tasks:"
+        )
+
+        done_label.grid(padx=5, pady=5, sticky=constants.W)
+
+        for task in self._course.tasks:
+            if task.state == 0:
+                self._initialize_task_entity(task)
+
+        remove_button = ttk.Button(
+            master=self._frame,
+            text="Remove course",
+            command=self._initialize_remove_course_popup
+        )
+
+        remove_button.grid(padx=5, pady=5, sticky=constants.E)
+
+        return_button = ttk.Button(
+            master=self._frame,
+            text="Return",
+            command=self._show_all_courses_view
+        )
+
+        return_button.grid(padx=5, pady=5, sticky=constants.W)
