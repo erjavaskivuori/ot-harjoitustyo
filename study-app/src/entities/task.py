7dde611c8440050a5a2bef802aa90c47b99747cd
erjav <erja.vaskivuori@helsinki.fi>
2022-12-11 16:52:14 +0200

    Added title and deadline parameters to constructor and renamed visibilty -> state

--- study-app/src/entities/task.py
+++ study-app/src/entities/task.py
@@ -1,9 +1,13 @@
 from entities.course import Course
+from datetime import date
 
 
 class Task:
-    def __init__(self, id: int, course: Course, description: str, visibility: int):
+    def __init__(self, id: int, course: Course, title: str, description: str, deadline: date, state: int):
         self.id = id
         self.course = course
+        self.title = title
         self.description = description
-        self.visibility = visibility
+        self.deadline = deadline
+        self.state = state
+        
