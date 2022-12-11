a847f6465e05a139c6098d25553625a4676514e6
erjav <erja.vaskivuori@helsinki.fi>
2022-12-11 17:03:02 +0200

    New columns in courseTasks taken into account in SQL-queries and when returning task-entity

--- study-app/src/repositories/task_repository.py
+++ study-app/src/repositories/task_repository.py
@@ -9,30 +9,28 @@ class TaskRepository:
 
         self._connection = connection
 
-    def create_task(self, course: Course, description):
+    def create_task(self, course: Course, title, description, deadline):
 
         cursor = self._connection.cursor()
-        cursor.execute("""INSERT INTO courseTasks (course_id, description,
-                        visibility) VALUES (?, ?, ?)""",
-                       [course.id, description, 1])
+        cursor.execute("""INSERT INTO courseTasks (course_id, title, description,
+                        deadline, state) VALUES (?, ?, ?, ?, ?)""",
+                       [course.id, title, description, deadline, 1])
         self._connection.commit()
-
-        return Task(cursor.lastrowid, course, description, 1)
 
     def get_tasks_by_course(self, course: Course):
 
         cursor = self._connection.cursor()
         cursor.execute(
-            "SELECT * FROM courseTasks WHERE visibility=1 AND course_id=?", [course.id])
+            "SELECT * FROM courseTasks WHERE course_id=?", [course.id])
         rows = cursor.fetchall()
 
-        return [Task(row[0], course, row[2], row[3]) for row in rows]
+        return [Task(row[0], course, row[2], row[3], row[4], row[5]) for row in rows]
 
     def remove_task(self, task: Task):
 
         cursor = self._connection.cursor()
         cursor.execute(
-            "UPDATE courseTasks SET visibility=0 WHERE id=?", [task.id])
+            "UPDATE courseTasks SET state=0 WHERE id=?", [task.id])
         self._connection.commit()
 
     def remove_all_tasks(self):
