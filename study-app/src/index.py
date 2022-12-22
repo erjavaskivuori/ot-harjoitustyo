3d730e7d0f253356b13041351e9a7f61f5187689
erjav <erja.vaskivuori@helsinki.fi>
2022-12-22 20:42:15 +0200

    Added theme

--- study-app/src/index.py
+++ study-app/src/index.py
@@ -1,10 +1,14 @@
-from tkinter import Tk
+from tkinter import Tk, ttk
 from ui.ui import UI
 
 
 def main():
     window = Tk()
     window.title("Study-app")
+
+    style = ttk.Style()
+    window.tk.call('source', 'src/theme/ttk-Breeze-0.6/breeze.tcl')
+    style.theme_use("Breeze")
 
     user_interface = UI(window)
     user_interface.start()
