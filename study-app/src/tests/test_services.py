66cf6771daa978660edfb548c46ed76f8abe1de4
erjav <erja.vaskivuori@helsinki.fi>
2022-12-24 04:08:40 +0200

    Removed unused code

--- study-app/src/tests/test_services.py
+++ study-app/src/tests/test_services.py
@@ -1,7 +1,6 @@
 import unittest
 from entities.user import User
 from entities.course import Course
-from entities.task import Task
 from services.study_app_service import StudyAppService, InvalidCredentialsError, UsernameExistsError
 
 
@@ -54,52 +53,16 @@ class FakeCourseRepository:
         self.courses = []
 
 
-class FakeTaskRepository:
-    def __init__(self, tasks=None):
-        self.tasks = tasks or []
-
-    def create_task(self, course: Course, title, description, deadline):
-
-        task = Task(1, course, title, description, deadline, 1)
-        self.tasks.append(task)
-
-    def get_tasks_by_course(self, course: Course):
-
-        course_tasks = course.tasks #list(filter(lambda task: task.course.name == course.name and
-                                    #task.state == 1, self.tasks))
-
-        for i in course_tasks:
-            if i.course.name == course.name:
-                print("sama")
-            elif i.state == 1:
-                print("joo")
-            else:
-                print("ei toimi")
-
-        return course_tasks
-
-    def remove_task(self, task: Task):
-        task.state = 0
-
-    def remove_all_tasks(self):
-        self.tasks = []
-
-
 class TestStudyAppServices(unittest.TestCase):
     def setUp(self):
         self.studyapp_service = StudyAppService(
             FakeUserRepository(),
-            FakeCourseRepository(),
-            FakeTaskRepository()
+            FakeCourseRepository()
         )
 
         self.test_user = User(1, "testuser", "password")
         self.course1 = Course(1, self.test_user, "course1", 1)
         self.course2 = Course(2, self.test_user, "course2", 1)
-        self.task1 = Task(1, self.course1, "title1",
-                          "description1", "11/11/2022", 1)
-        self.task2 = Task(2, self.course2, "title2",
-                          "description2", "12/12/2022", 1)
 
     def login(self, user):
         self.studyapp_service.create_user(user.username, user.password)
